9930255586e402726ef5c0bb5096923784b285ae
ckelling <ckelling@vt.edu>
2018-01-31 20:24:36 -0500

    finishing up problem 2

--- homework_1/src/cek32HW1.R
+++ homework_1/src/cek32HW1.R
@@ -12,6 +12,8 @@ library(tictoc)
 library(optimr)
 library(ggplot2)
 library(xtable)
+library(tictoc)
+library(RSpectra)
 
 ###
 ### Problem 1
@@ -150,31 +152,40 @@ d2 <- lambda[2]-lambda[3]
 ###
 
 #now I will do this using Monte Carlo methods
-tic()
-m <- 10000
-n <- 10000 #sample size
-d1 <- NULL
-d2 <- NULL
-for(i in 1:n){
-  m <- 10
-  R <- matrix(rnorm(m^2,0,1), nrow = m, ncol = m)
-  Sigma <- R%*%t(R)
-  eigen_decomp <- eigen(Sigma)
-  lambda <- eigen_decomp$values[(length(eigen_decomp$values)-2):length(eigen_decomp$values)]
-  d1_new <- lambda[3]-lambda[2]
-  d2_new <- lambda[2]-lambda[1]
-  d1 <- rbind(d1,d1_new)
-  d2 <- rbind(d2,d2_new)
+m <- c(1000)#,1000)#,10000) skip 10,000 for now
+n <- 1000 #Monte Carlo sample size
+d1 <- rep(NA,n)
+d2 <- rep(NA,n)
+df <- NULL
+for(j in m){
+  print(paste(j, "**********************"))
+  d1 <- rep(NA,n)
+  d2 <- rep(NA,n)
+  for(i in 1:n){
+    #if(i %% 100 == 0){ print (i)}
+    print(i)
+    R <- matrix(rnorm(j^2,0,1), nrow = j, ncol = j)
+    Sigma <- R%*%t(R)
+    #eigen_decomp <- eigen(Sigma, only.values = T)
+    eigen_decomp <- eigs(Sigma, 3, opts=list(retvec=FALSE))
+    #smallest <- eigs(Sigma, 1, which = "LM", sigma = 0)$values
+    # https://cran.r-project.org/web/packages/RSpectra/vignettes/introduction.html
+    lambda <- eigen_decomp$values[1:3]
+    d1_new <- lambda[3]-lambda[2]
+    d2_new <- lambda[2]-lambda[1]
+    d1[i] <- d1_new
+    d2[i] <- d2_new
+  }
+  df <- cbind(df, d1,d2)
 }
-toc()
-
+dfb <- df
 #expected value of d1 and d2
 mean(d1) # m=100: 9.69232    , m=1,000: 9.591571    , m=10,000: 9.537369
 mean(d2) # m=100: 6.271687   , m=1,000: 6.284819    , m=10,000: 6.324104
 
 #Monte Carlo Standard Errors
-sd(d1)  # m=100: 5.546734   , m=1,000: 5.392687    , m=10,000: 5.470581
-sd(d2)  # m=100: 3.493466   , m=1,000: 3.499043    , m=10,000: 3.52086
+sd(d1)/sqrt(n)  # m=100: 5.546734   , m=1,000: 5.392687    , m=10,000: 5.470581
+sd(d2)/sqrt(n)  # m=100: 3.493466   , m=1,000: 3.499043    , m=10,000: 3.52086
 
 #plot approximate density plots for d1, d2
 plot(density(d1), "Density of d1 at m = 100")
@@ -185,59 +196,87 @@ plot(density(d2), "Density of d2 at m = 100")
 ###
 
 #What is the expected value of the smallest eigenvalue?
-tic()
-m <- 10000
-n <- 10000 #sample size
-sm_eigen <- NULL
-for(i in 1:n){
-  R <- matrix(rnorm(m^2,0,1), nrow = m, ncol = m)
-  Sigma <- R%*%t(R)
-  eigen_decomp <- eigen(Sigma)
-  sm_new <- eigen_decomp$values[length(eigen_decomp$values)]
-  sm_eigen <- c(sm_eigen, sm_new)
+m <- c(100,1000)
+n <- 1000 #Monte Carlo sample size
+sm <- rep(NA,n)
+df <- NULL
+for(j in m){
+  print(paste(j, "**********************"))
+  sm <- rep(NA,n)
+  for(i in 1:n){
+    #if(i %% 100 == 0){ print (i)}
+    print(i)
+    R <- matrix(rnorm(j^2,0,1), nrow = j, ncol = j)
+    Sigma <- R%*%t(R)
+    #eigen_decomp <- eigen(Sigma, only.values = T)
+    #eigen_decomp <- eigs(Sigma, 3, opts=list(retvec=FALSE))
+    smallest <- eigs(Sigma, 1, which = "LM", sigma = 0,opts=list(retvec=FALSE))$values
+    # https://cran.r-project.org/web/packages/RSpectra/vignettes/introduction.html
+    sm[i] <- smallest[1]
+  }
+  df <- cbind(df,sm)
 }
-toc()
 
 #expected value of d1 and d2
-mean(sm_eigen) # m=100: 9.69232    , m=1,000: 9.591571    , m=10,000: 9.537369
+mean(df[,1]) #m=100
+mean(df[,2]) #m=1000
 
 #Monte Carlo Standard Errors
-sd(sm_eigen)  # m=100: 5.546734   , m=1,000: 5.392687    , m=10,000: 5.470581
+sd(df[,1])/sqrt(n) #m=100
+sd(df[,2])/sqrt(n) #m=1000
 
-#plot approximate density plots for d1, d2
-plot(density(sm_eigen), "Density of smallest eigen at m = 100")
+save(df, file = "C:/Users/ckell/OneDrive/Penn State/2017-2018/01_Spring/540/statistical_computing_540/homework_1/data/prob_2b.Rdata")
+
+#plot approximate density plots for smallest eigen value
+plot(density(df[,1]), "Density of smallest eigen at m = 100")
+plot(density(df[,2]), "Density of smallest eigen at m = 1,000")
 
 ###
 ###  2c)
 ###
 
 #Now study how the expected smallest eigenvalue changes as a function of m. 
-#(i) Plot approximate E(lambda1), expected value of smallest eigenvalue, versus m. 
-a <- 1:100
-b <- 1
-m <- a * b
+#(i) Plot approximate E(lambda_m), expected value of smallest eigenvalue, versus m. 
+m <- c(1:100 * 10,3:6*500) #1:100 * 10,
+test <- 1:100 * 10
+#exp_val_vec2 <- exp_val_vec
 exp_val_vec <- NULL
-tic()
+n <- 100 #sample size
+
 for(j in m){
-  n <- 10000 #sample size
-  sm_eigen <- NULL
+  print(paste(j, "**********************"))
+  sm <- rep(NA,n)
   for(i in 1:n){
+    #if(i %% 100 == 0){ print (i)}
+    print(i)
     R <- matrix(rnorm(j^2,0,1), nrow = j, ncol = j)
     Sigma <- R%*%t(R)
-    eigen_decomp <- eigen(Sigma)
-    sm_new <- eigen_decomp$values[length(eigen_decomp$values)]
-    sm_eigen <- c(sm_eigen, sm_new)
+    #eigen_decomp <- eigen(Sigma, only.values = T)
+    #eigen_decomp <- eigs(Sigma, 3, opts=list(retvec=FALSE))
+    smallest <- eigs(Sigma, 1, which = "LM", sigma = 0,opts=list(retvec=FALSE))$values
+    # https://cran.r-project.org/web/packages/RSpectra/vignettes/introduction.html
+    sm[i] <- smallest[1]
   }
-  exp_val <- mean(sm_eigen)
+  exp_val <- mean(sm)
   exp_val_vec <- c(exp_val_vec, exp_val)
-  print(m[j])
 }
-toc()
-plot(x= m,y= exp_val_vec)
+
+###
+
+###
+
+prob_2 <- data.frame(m, exp_val_vec)
+colnames(prob_2) <- c("m", "expected_value")
+prob_2$m <- as.numeric(as.character(prob_2$m))
+prob_2$expected_value <- as.numeric(as.character(prob_2$expected_value))
+
+ggplot(prob_2) + geom_line(aes(x=m, y = expected_value), size = 1.2)+
+  labs(title = "m vs expected value of smallest eigenvector", ylab = "expected value")
 
 #(ii) Report your grid of m values. (iii) What is the largest m value for which you 
 #     approximated the expectation?
 
+exp_val_vec_2 <- c(0.0733591536, 0.0379156155, 0.0198245616, 0.0180753816, 0.0103755242, 0.0089777114, 0.0096156783, 0.0102288524, 0.0069491758, 0.0062841157, 0.0053903295, 0.0063654489, 0.0050404595, 0.0052720499, 0.0054420552, 0.0035475238, 0.0039151589, 0.0041045611, 0.0040739881, 0.0037872857, 0.0037378989, 0.0035142535, 0.0026135692, 0.0027201996, 0.0027336333, 0.0027757366, 0.0022619196, 0.0019878161, 0.0016583414, 0.0021004911, 0.0017371396, 0.0017891204, 0.0022129804, 0.0017180198, 0.0024197094, 0.0022395280, 0.0015340470, 0.0012885595, 0.0018150200, 0.0018829293, 0.0017238653, 0.0021366696, 0.0019007053, 0.0014812419, 0.0016249169, 0.0014746681, 0.0015488306, 0.0015422615, 0.0014832698, 0.0013745350, 0.0010942752, 0.0012683243, 0.0012176643, 0.0010465356, 0.0013874523, 0.0009151464, 0.0009779324, 0.0011047335, 0.0011135144, 0.0011581533 0.0013261632 0.0011842986 0.0009418504 0.0008637009 0.0012042640 0.0014792420 0.0009825543 0.0009585732 0.0008142311 0.0008808660 0.0009690631 0.0012030432 0.0008774910 0.0009821160 0.0007181296 0.0009557500 0.0009308187 0.0011616895 0.0009327619 0.0007620769 0.0010152202 0.0009325263 0.0007143623 0.0006568169 0.0007831074 0.0008342921 0.0007174419 0.0009398580 0.0008389230 0.0007639229 0.0006491160 0.0007129268 0.0006939538 0.0007379282 0.0007542634 0.0006987829 0.0008465385 0.0006206839 0.0006725681 0.0008509256
 
 
 ###
