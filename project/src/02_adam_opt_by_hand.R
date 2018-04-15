20f48fb4ed1b9b80da82cee0113999d1501e9d59
ckelling <ckelling@vt.edu>
2018-04-15 15:00:02 -0400

    initial commit, adam by hand

---
+++ project/src/02_adam_opt_by_hand.R
@@ -0,0 +1,9 @@
+###
+### Claire Kelling
+### SODA 540
+### Final Project
+###
+### Created 3/20/18 for final project code on Adam optimization
+### Here, we code the adam optimizer by hand, and assess for possible difficulties.
+### 
+
