53b41fb85c3901031c2b1f789a0a077e35be5272
ckelling <ckelling@vt.edu>
2018-04-17 20:05:04 -0400

    without backtracking

--- project/src/03_adam_opt_logreg.R
+++ project/src/03_adam_opt_logreg.R
@@ -9,27 +9,26 @@
 ### In other words, this is trying to find the Gamma parameters
 ### 
 
-# creating function for probability (first derivative)
-p_funct <- function(X, beta){
-  # compute vector p of probabilities for logistic regression with logit link
-  X <- as.matrix(X)
-  beta <- as.vector(beta)
-  p <- exp(X %*% beta) / (1 + exp(X %*% beta))
-  return(p)
+obj_fun = function(theta){
+  x.b = as.vector(X%*%theta)
+  return( -sum(y*x.b - log( 1 + exp(x.b))))  
 }
 
-# creating function for log-likelihood
-log_lik <- function(y, p){
-  # bernoulli log likelihood function
-  # input:   vectors: y = 0/1 outcome; p = probabilities
-  # output: log-likelihood l, a scalar
-  one_vec <- rep(1,nrow(prob1))
-  l <- t(y) %*% log(p) + t(one_vec - y) %*% log(1 - p)
-  return(l)
+Score.SGD <- function(theta,batch.size, index){
+  loop_sum = matrix(0,ncol=1,nrow=2)
+  
+  for(i in index){
+    exp.i = exp(crossprod(X[i,],theta))
+    const.i_num = as.numeric(exp.i)
+    const.i_den = as.numeric((1+exp.i))
+    
+    loop_sum = loop_sum + X[i,] * (Y[i] - const.i_num / const.i_den )
+  }
+  
+  return(-loop_sum)
 }
 
-
-adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
+adam_opt <- function(data, step, beta1, beta2, eps1, eps2, init, maxit){  
   
   #initializations of iteration counts and other variables, as in paper
   t <- 0
@@ -42,20 +41,20 @@ adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
   adam.hist <- data.frame(t, lr_t, mean, var)
   theta.hist <- theta_vec
   
-  #give data an intercept
-  data <- cbind(1, data)
-  
   #split data into x and y variables
-  x_var <- data[,1:(ncol(data)-1)]
-  x_var <- as.matrix(x_var)
-  y_var <- data[,ncol(data)]
-  diff_theta <- 10
+  data<- as.data.frame(data)
+  X <- data[,1:(ncol(data)-1)]
+  X <- as.matrix(X)
+  y <- data[,ncol(data)]
+  diff_theta <- 10; diff_stop <- T; step_stop <- T #arbitrarily high number and false
   
   #now, we will create the updates according to the algorithm
-  while((diff_theta > eps2) & (t < maxit)){
+  while((diff_theta > eps2) & (t < maxit) & diff_stop & step_stop){
     
     #keep the prior theta to assess for convergence
     theta_prior <- theta_vec
+    theta_vec <- as.vector(theta_vec)
+    theta_prior <- as.vector(theta_prior)
     
     #update time step
     t <- t+1
@@ -64,14 +63,13 @@ adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
     #find the stochastic row that we will use
     stoch_iter <- sample(1:nrow(data), 1)
     
-    #need for calculating gradient for linear regression
-    #theta_vec <- as.numeric(theta_vec$theta_vec)
-    error <- (t(x_var[stoch_iter,]) %*% (theta_vec)) - y_var[stoch_iter]
+    #need for calculating gradient for logistic regression
+    Score.eval = Score.SGD(theta_prior,1, stoch_iter)
     
+    # update theta
     for(k in 1:length(theta_vec)){
       #Get gradients w.r.t. stochastic objective at timestep t
-      #k <- 2
-      grad <- error * x_var[stoch_iter, k]
+      grad <- Score.eval[k]
       
       #current estimate of theta
       theta <- theta_vec[k]
@@ -84,7 +82,7 @@ adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
       #compute lr_t
       num <- 1 - beta1^t
       denom <- 1 - beta2^t
-      lr_t <- lr * sqrt(num)/denom
+      lr_t <- step * sqrt(num)/denom
       
       #Compute bias-corrected first and second moment estimate
       #Update parameters
@@ -92,9 +90,16 @@ adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
       theta_vec[k] <- theta
     }
     
+    ll.prev = obj_fun(theta_prior)
+    ll.new = obj_fun(theta_vec)
+    
+    
+    diff_stop = max( abs(theta_prior-theta_vec)/ abs(theta_prior)) > 1e-10  
+    ll_stop   = abs( ll.new-ll.prev ) / abs(ll.prev)  > 1e-10 
+    
     #Create output vector
-    adam.hist   <- rbind(adam.hist, c(t, lr_t, mean, var))
-    theta.hist <- rbind(theta.hist, theta_vec)
+    adam.hist   <- rbind(adam.hist, c(t))
+    theta.hist <- rbind(theta.hist, c(theta_vec))
     
     diff_theta <- max(abs(theta_prior-theta_vec))
   }
@@ -104,27 +109,29 @@ adam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
   output$iter        <- t - 1
   output$adam.hist   <- adam.hist
   output$theta.hist  <- theta.hist
+  output$backtrack.iter <- backtrack.iter
   
   return(output)
-  
 }
 
-#try it out!
+#test the adam algorithm
+prob1 <- fread("http://personal.psu.edu/muh10/540/data/logReg.dat")
 beta1 <- 0.9
 beta2 <- 0.999
 eps1 <- 0.0000001 #for the algorithm
-eps2 <- 0.0001 #convergence criteria
+eps2 <- 1e-6 #convergence criteria
 maxit <- 500
 lr <- 0.001
+data <- prob1
 
-adam_out <- adam_opt(tr_dat,lr, beta1, beta2, eps1, eps2, init, maxit)
-theta_hist <- test$theta.hist
+adam_out <- adam_opt(data,lr, beta1, beta2, eps1, eps2, init, maxit)
+theta_hist <- adam_out$theta.hist
 
 ##
 ## Now, I would like to apply Nesterov-accelerated adaptive moment estimation
 ##
 
-nadam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
+nadam_opt <- function(data, step, beta1, beta2, eps1, eps2, init, maxit){  
   
   #initializations of iteration counts and other variables, as in paper
   t <- 0
@@ -136,25 +143,24 @@ nadam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
   nadam.hist <- data.frame(t, mean, var)
   theta.hist <- theta_vec
   
-  #give data an intercept
-  #data <- cbind(1, tr_dat) #need to change back to data
-  data <- cbind(1, data)
-  
   #split data into x and y variables
-  x_var <- data[,1:(ncol(data)-1)]
-  x_var <- as.matrix(x_var)
-  y_var <- data[,ncol(data)]
-  diff_theta <- 10
+  data<- as.data.frame(data)
+  X <- data[,1:(ncol(data)-1)]
+  X <- as.matrix(X)
+  y <- data[,ncol(data)]
+  diff_theta <- 10; diff_stop <- T; step_stop <- T #arbitrarily high number and false
   
   #momentum scheduler
   i <- 1:maxit
   beta1.t.vec <- beta1*(1-0.5*0.96^(i/250))
   
   #now, we will create the updates according to the algorithm
-  while((diff_theta > eps2) & (t < maxit)){
+  while((diff_theta > eps2) & (t < maxit) & diff_stop & step_stop){
     
     #keep the prior theta to assess for convergence
     theta_prior <- theta_vec
+    theta_vec <- as.vector(theta_vec)
+    theta_prior <- as.vector(theta_prior)
     
     #update time step
     t <- t+1
@@ -163,20 +169,16 @@ nadam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
     #find the stochastic row that we will use
     stoch_iter <- sample(1:nrow(data), 1)
     
-    #need for calculating gradient for linear regression
-    #theta_vec <- as.numeric(theta_vec$theta_vec)
-    error <- (t(x_var[stoch_iter,]) %*% (theta_vec)) - y_var[stoch_iter]
+    #need for calculating gradient for logistic regression
+    Score.eval = Score.SGD(theta_prior,1, stoch_iter)
     
+    # update theta
     for(k in 1:length(theta_vec)){
-      #for testing purposes
-      #k<-2
-      
       #current estimate of theta
       theta <- theta_vec[k]
       
       #Get gradients w.r.t. stochastic objective at timestep t
-      grad <- error * x_var[stoch_iter, k]
-      
+      grad <- Score.eval[k]
       g_hat <- (grad)/ (1-prod(beta1.t.vec[1:t]))
       
       #Update biased first moment estimate
@@ -197,9 +199,16 @@ nadam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
       theta_vec[k] <- theta
     }
     
+    ll.prev = obj_fun(theta_prior)
+    ll.new = obj_fun(theta_vec)
+    
+    
+    diff_stop = max( abs(theta_prior-theta_vec)/ abs(theta_prior)) > 1e-10  
+    ll_stop   = abs( ll.new-ll.prev ) / abs(ll.prev)  > 1e-10 
+    
     #Create output vector
-    nadam.hist   <- rbind(adam.hist, c(t, mhat, var_hat))
-    theta.hist <- rbind(theta.hist, theta_vec)
+    nadam.hist   <- rbind(nadam.hist, c(t))
+    theta.hist <- rbind(theta.hist, c(theta_vec))
     
     diff_theta <- max(abs(theta_prior-theta_vec))
   }
@@ -207,23 +216,23 @@ nadam_opt <- function(data, lr, beta1, beta2, eps1, eps2, init, maxit){
   output <- list()
   output$theta.final  <- theta_vec
   output$iter        <- t - 1
-  output$adam.hist   <- nadam.hist
+  output$nadam.hist   <- nadam.hist
   output$theta.hist  <- theta.hist
+  output$backtrack.iter <- backtrack.iter
   
   return(output)
-  
 }
 
-#testing it out!
+#Running Nadam algorithm
 beta1 <- 0.9
 beta2 <- 0.999
 eps1 <- 0.0000001 #for the algorithm
-eps2 <- 0.0001 #convergence criteria
-maxit <- 500
+eps2 <- 1e-6 #convergence criteria
+maxit <- 700
 lr <- 0.001
 
-test <- nadam_opt(tr_dat,lr, beta1, beta2, eps1, eps2, init, maxit)
-theta_hist <- test$theta.hist
+nadam_output <- nadam_opt(data,lr, beta1, beta2, eps1, eps2, init, maxit)
+theta_hist <- nadam_output$theta.hist
 
 
 ###
@@ -231,7 +240,7 @@ theta_hist <- test$theta.hist
 ###
 
 tic()
-adam_out <- adam_opt(tr_dat,lr, beta1, beta2, eps1, eps2, init, maxit)
+adam_out <- adam_opt(data,0.1, beta1, beta2, eps1, eps2, init, maxit)
 adam_time <- toc()
 adam_time <- adam_time$toc-adam_time$tic
 
@@ -248,12 +257,14 @@ ggplot(data=theta_df, aes(x=ind,y=est, group=coeff, col = coeff)) +
   #geom_line()+
   geom_point()+labs(title = "Adam Algorithm")
 
+adam_out$theta.final
+
 ###
 ### Measurements for Nadam
 ###
 
 tic()
-nadam_out <- nadam_opt(tr_dat,lr, beta1, beta2, eps1, eps2, init, maxit)
+nadam_out <- nadam_opt(data,lr, beta1, beta2, eps1, eps2, init, maxit)
 nadam_time <- toc()
 nadam_time <- nadam_time$toc-nadam_time$tic
 
