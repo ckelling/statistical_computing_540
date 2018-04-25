957fe9d31ffd0223e9cebdbedee4562b7ab11d61
ckelling <ckelling@vt.edu>
2018-04-25 09:49:16 -0400

    analysis of spam data

--- project/src/12_spam_app.R
+++ project/src/12_spam_app.R
@@ -6,9 +6,15 @@
 ### Created 4/15/18 for final project use of spam dataset
 ### 
 
+#https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.DOCUMENTATION
+#https://archive.ics.uci.edu/ml/datasets/spambase
+#https://www.kaggle.com/c/cs-189-logistic-regression2/data
 spam_dat <- fread("https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data")
 
 source(file = "C:/Users/ckell/OneDrive/Penn State/2017-2018/01_Spring/540/statistical_computing_540/project/src/00_opt_functions.R")
+
+data <- spam_dat[,-(5:57)]
+#data <- spam_dat
 
 #try testing glm
 mod <- glm(V58 ~.,family=binomial(link='logit'),data=spam_dat)
@@ -21,7 +27,7 @@ eps2 <- 1e-6 #convergence criteria
 maxit <- 1000
 step <- 1#step size of 1 helps speed
 
-data<- as.data.frame(spam_dat)
+data<- as.data.frame(data)
 X <- data[,1:(ncol(data)-1)]
 X <- as.matrix(X)
 y <- data[,ncol(data)]
@@ -46,4 +52,9 @@ theta_df$est <- as.numeric(as.character(theta_df$est))
 ggplot(data=theta_df, aes(x=ind,y=est, group=coeff, col = coeff)) +
   geom_line()+
   geom_point()+labs(title = "Stochastic Gradient Descent")
+sgd_spam$theta.final
 
+# [1,]  0.2277371
+# [2,] -0.1290784
+# [3,]  0.2052651
+# [4,]  3.4976576
